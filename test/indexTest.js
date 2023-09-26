c6ef8863fbe41791c061a7134e5ec50aa06c32c8
Earl <earlmunyua96@gmail.com>
2023-09-26 23:48:48 +0300

    Javascript 1a

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,20 +3,20 @@ require ( './root.js' );
 const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
-  describe("Name", () => {
+  describe("name", () => {
     it('returns "Susan"', () => {
       expect(name).toEqual("Susan");
     });
   });
 
-  describe("Height", () => {
-    it("is less than 40 and greater than 0", () => {
+  describe("height", () => {
+    it("is less than 74 and greater than 0", () => {
       expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
+      expect(height).toBeLessThan(74)
     });
   });
 
-  describe("Message", () => {
+  describe("message", () => {
     it("gives the name and height", () => {
       expect(message).toInclude(name);
       expect(message).toInclude(height);
